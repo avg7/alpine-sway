ba382982f5118eb28bd889abb473056ec1f131e0
Meiko Grunewald <73403788+avg7@users.noreply.github.com>
2021-03-09 18:56:19 +0100

    Add files via upload

--- server-apk.sh
+++ server-apk.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apk add logcheck grep bash logtail perl run-parts macchanger iptables audit sudo
+apk add openvpn openresolv logcheck grep bash logtail perl run-parts macchanger iptables audit sudo
