e731d705755869a1e510379ae8eb65c1a9cff1b1
Meiko Grunewald <73403788+avg7@users.noreply.github.com>
2021-03-09 18:58:01 +0100

    Add files via upload

--- sway-apk.sh
+++ sway-apk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apk add audit eudev libinput sway sway-doc xorg-server-xwayland swaylock swayidle ttf-freefont mesa mesa-dri-gallium mesa-egl mesa-gbm mesa-gl mesa-glapi mesa-gles mc sudo macchanger firefox-esr libc6-compat gnupg iptables alacritty logcheck grep bash logtail perl run-parts;
-echo "Enable Edge Testing Repository and run: apk add bemenu"
+echo "Enable Edge Testing Repository and run: apk update && apk add bemenu"
